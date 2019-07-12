f1c1c17aef8acc0ee1eefdbd6eff3b0c36118f4d
Sherman Peng <ypeng7@mail.ccsf.edu>
2019-07-11 20:41:39 -0700

    Updates

---
+++ precache-manifest.0693a883f90bae3058273b8cd2c524cc.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcdd40a2cb3399146de21c74a758933f",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "35785cddd752aaac96b6",
+    "url": "/website/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "749b34967f455b1941e0",
+    "url": "/website/static/css/main.b52b510b.chunk.css"
+  },
+  {
+    "revision": "35785cddd752aaac96b6",
+    "url": "/website/static/js/2.9a9ea3e9.chunk.js"
+  },
+  {
+    "revision": "749b34967f455b1941e0",
+    "url": "/website/static/js/main.446c9073.chunk.js"
+  },
+  {
+    "revision": "d15da1c5dbbceb93f3a7",
+    "url": "/website/static/js/runtime~main.de462641.js"
+  },
+  {
+    "revision": "cb9a10e53a6094da054aaa328fb45cd8",
+    "url": "/website/static/media/NY-location-project.cb9a10e5.png"
+  },
+  {
+    "revision": "3415835d1364828448b0735c07028eda",
+    "url": "/website/static/media/Resume.3415835d.pdf"
+  },
+  {
+    "revision": "8262057119c963741b868e0d8dd68dd3",
+    "url": "/website/static/media/economica-bold.82620571.ttf"
+  },
+  {
+    "revision": "fea5f5eb01bd3a8bbfbbba51e0acb058",
+    "url": "/website/static/media/photo.fea5f5eb.jpg"
+  },
+  {
+    "revision": "cf4449c1884598b292630aa9f4007c19",
+    "url": "/website/static/media/roboto-condensed.light.cf4449c1.ttf"
+  },
+  {
+    "revision": "86cfd4bcb0857cd287ec8107243a71ad",
+    "url": "/website/static/media/tobacco-permit-project.86cfd4bc.png"
+  }
+]);
